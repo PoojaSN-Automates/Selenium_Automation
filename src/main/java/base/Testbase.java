507b945ff96ef202f402d1a790bc060691d96b3d
PoojaSN-Automates <poojasn9999@gmail.com>
2026-05-28 16:50:35 +0530

    Update Testbase.java

--- src/main/java/base/Testbase.java
+++ src/main/java/base/Testbase.java
@@ -1,6 +1,5 @@
 package base;
 
-
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,106 +7,125 @@ import java.util.Map;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import utils.ConfigReader;
 
-
-//Initialize WebDriver and launch browser here
 public class Testbase {
 
-	 // ThreadLocal Driver
-	public static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-	
-	// Config object
-	ConfigReader config;
-	
-	  // Get Driver
-	public static WebDriver getDriver() {
+    // ThreadLocal Driver (for parallel execution)
+    public static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    // Config object
+    ConfigReader config;
+
+    // Get Driver
+    public static WebDriver getDriver() {
         return driver.get();
     }
-	
-	@BeforeMethod
-	public void setUp() {
-		
-// Initialize config
-		config= new ConfigReader();
-		
-		String browser = config.getBrowser();
+
+    @BeforeMethod
+    public void setUp() {
+
+        // Initialize config
+        config = new ConfigReader();
+
+        String browser = System.getProperty("browser", config.getBrowser());
         String url = config.getUrl();
-        
-        // Chrome Browser
-        if(browser.equalsIgnoreCase("chrome")) {
+
+        WebDriver localDriver = null;
+
+        // CHROME
        
- // Browser options
-		ChromeOptions options= new ChromeOptions();// these are commonly used adding capabilities for chrome browser to avoid any issues on browser set up
-		options.addArguments("--start-maximized");
-		options.addArguments("--disable-notifications");
-		options.addArguments("--disable-extensions");
-		options.addArguments("--remote-allow-origins=*");
-		options.addArguments("--guest");
-		
-		  // Headless support
-        if(config.isHeadless()) {
+        if (browser.equalsIgnoreCase("chrome")) {
 
-            options.addArguments(
-                    "--headless=new");
+            ChromeOptions options = new ChromeOptions();
+
+            options.addArguments("--start-maximized");
+            options.addArguments("--disable-notifications");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--guest");
+
+            // Headless support
+            if (config.isHeadless()) {
+                options.addArguments("--headless=new");
+                options.addArguments("--window-size=1920,1080");
+            }
+
+            // Disable password manager
+            Map<String, Object> prefs = new HashMap<>();
+            prefs.put("credentials_enable_service", false);
+            prefs.put("profile.password_manager_enabled", false);
+            prefs.put("autofill.profile_enabled", false);
+            prefs.put("autofill.credit_card_enabled", false);
+
+            options.setExperimentalOption("prefs", prefs);
+
+            localDriver = new ChromeDriver(options);
         }
-		
-// Disable password manager
-Map<String, Object> prefs =
-        new HashMap<>();
 
-prefs.put(
-        "credentials_enable_service",
-        false);
+       
+        // FIREFOX
+       
+        else if (browser.equalsIgnoreCase("firefox")) {
 
-prefs.put(
-        "profile.password_manager_enabled",
-        false);
+            FirefoxOptions options = new FirefoxOptions();
 
-prefs.put(
-        "autofill.profile_enabled",
-        false);
+            if (config.isHeadless()) {
+                options.addArguments("--headless");
+            }
 
-prefs.put(
-        "autofill.credit_card_enabled",
-        false);
-
-options.setExperimentalOption(
-        "prefs",
-        prefs);
-		
-//Initialize driver
-		driver.set(new ChromeDriver(options));
-		
+            localDriver = new FirefoxDriver(options);
         }
-        // Invalid browser
+
+        
+        // EDGE
+       
+        else if (browser.equalsIgnoreCase("edge")) {
+
+            EdgeOptions options = new EdgeOptions();
+
+            options.addArguments("--start-maximized");
+
+            if (config.isHeadless()) {
+                options.addArguments("--headless=new");
+            }
+
+            localDriver = new EdgeDriver(options);
+        }
+
+    
+        // INVALID BROWSER
+       
         else {
-
-            throw new RuntimeException(
-                    "Browser not supported");
+            throw new RuntimeException("Browser not supported: " + browser);
         }
-        
-     // Maximize browser
-        getDriver().manage()
-        .window().maximize();
 
+        // Set driver in ThreadLocal
+        driver.set(localDriver);
 
+        // Common settings
+        getDriver().manage().timeouts()
+                .implicitlyWait(Duration.ofSeconds(config.getTimeout()));
 
-        // Implicit wait
-        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(config.getTimeout()));
-        
-     // Open URL
+        getDriver().manage().window().maximize();
+
         getDriver().get(url);
-	}
-	
-	@AfterMethod
-	public void tearDown() {
-		if(getDriver()!=null) {
-			getDriver().quit();
-			 driver.remove(); //(prevents memory leak)
-		}
-	}
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+        if (getDriver() != null) {
+            getDriver().quit();
+            driver.remove(); // prevents memory leak in parallel runs
+        }
+    }
 }
